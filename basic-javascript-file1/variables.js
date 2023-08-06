967d414e270fa82dd0a4ad0056619c46de22a1d5
nitishrajpandey <nitishrajpandey0707@gmail.com>
2023-08-06 12:16:23 +0530

    learn variables

---
+++ basic-javascript-file1/variables.js
@@ -0,0 +1,47 @@
+//you can use variable as a camel case
+//we can never change the value of const 
+const userID=20645;
+
+// userID=20655;  this is error we cant change const value
+
+console.log(userID); //to print const variable value
+
+
+// **********************************************************************************************
+
+
+let UserNme="nitish raj"; // we can change the value and access the value at a block level
+
+console.log(UserNme);
+
+UserNme="navneet raj"; //we can change the value 
+
+console.log(UserNme);
+
+
+// ************************************************************************************************
+ 
+// avoid using var **
+
+var age=20; // we can change and access value form any where 
+
+console.log(age);
+
+age=21; // change the value
+
+console.log(age);
+
+
+// *********************************************************************************************
+
+// avoid this type of variable initilization
+
+email = "nitishraj@gmail.com"; // we can declear variable this tyle also
+
+console.log(email);
+
+email="navneetraj@gmail.com"
+
+console.log(email);
+
+
