d56ef7b7e0b3989bd021ac9cb7dee78e5e99e5e7
nitishrajpandey <nitishrajpandey0707@gmail.com>
2023-08-06 11:33:01 +0530

    testing file

---
+++ test.js
@@ -0,0 +1,4 @@
+// testing file to run properly or not
+// node -v       => to check the version of node js
+// node fileName.js  => to run the javascript file in terminal 
+console.log("hello nitish");
