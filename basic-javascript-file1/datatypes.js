d3e4a05521e298d9e5b95ed55ca54affbd3ffa80
nitishrajpandey <nitishrajpandey0707@gmail.com>
2023-08-06 13:13:02 +0530

    datatypes learning practice

---
+++ basic-javascript-file1/datatypes.js
@@ -0,0 +1,52 @@
+// *********************************************************************************
+
+"use strict" // when we apply this we strictly follow latest javascript rule
+
+// *******************************************************************************
+
+// alert(3+3)  // we are using nodejs not browser this function work on browser console
+
+// *********************************************************************************
+
+console.log("hello"); console.log("nitish") // this is not good code readiability
+
+//*********************************************************************************
+
+console.log(typeof "hello"); // typeof show the type of value or variable  
+// output => string
+
+// *********************************************************************************
+                                   // Datatypes
+let userId=12425; // number
+console.log(userId);
+console.log(typeof userId);
+
+
+let bigNumber=5n; //bigInt => n
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+
+let name="nitish" // string => ""
+console.log(name);
+console.log(typeof name);
+
+
+let boolean= true; //boolean => true/false
+console.log(boolean);
+console.log(typeof boolean);
+
+
+let value1=null; //null (standalone value)
+console.log(value1);
+console.log(typeof value1);
+
+
+let value2=undefined; //undefined (you not give the value)
+console.log(value2);
+console.log(typeof value2);
+
+
+// symbol => uniquesness 
+
+// object => refrence datatypes
